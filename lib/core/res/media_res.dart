786a7aff7f01cf760df3b1276f32d8705333950a
Gantogtokh <oganaa2472@gmail.com>
2025-08-22 18:13:08 -0500

    constrains class

---
+++ lib/core/res/media_res.dart
@@ -0,0 +1,9 @@
+
+class MediaRes {
+  const MediaRes._();
+  static const _baseIcons = 'assets/icons';
+  static const atom = '$_baseIcons/atom.png';
+  static const _baseImages = 'assets/images';
+  static const onBoardingBackground = '';
+  static const pageUnderConstruction = '';
+}
